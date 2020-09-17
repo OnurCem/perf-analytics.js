5c192a42ca335de54dba23517c27014591227d76
Onur Cem <onurcemsenel@gmail.com>
2020-09-17 20:33:50 +0300

    feat: call calculation functions on init

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,16 @@
+import { measureFCP } from './modules/fcp/measureFCP';
+import { measureTTFB } from './modules/ttfb/measureTTFB';
+import { measureDOMLoad } from './modules/dom-load/measureDOMLoad';
+import { measureWindowLoad } from './modules/window-load/measureWindowLoad';
+import { measureResourceTimings } from './modules/resource-timing/measureResourceTimings';
+
 export const init = (apiKey: string): void => {
   // eslint-disable-next-line no-console
   console.log('Initialized with API key:', apiKey);
+
+  measureFCP();
+  measureTTFB();
+  measureDOMLoad();
+  measureWindowLoad();
+  measureResourceTimings();
 };
