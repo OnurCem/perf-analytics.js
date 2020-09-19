29fdcc48d8d5e65258b3ba0a3f8578eab27f1d27
Onur Cem <onurcemsenel@gmail.com>
2020-09-20 02:48:08 +0300

    feat: implement sending metrics to API

--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,50 @@ import { measureTTFB } from './modules/ttfb/measureTTFB';
 import { measureDOMLoad } from './modules/dom-load/measureDOMLoad';
 import { measureWindowLoad } from './modules/window-load/measureWindowLoad';
 import { measureResourceTimings } from './modules/resource-timing/measureResourceTimings';
+import { MetricModel } from './models/MetricModels';
+import { postMetric } from './api/collectApi';
+import { MAX_RETRY_COUNT_ON_NETWORK_FAULT, REQUEST_RETRY_DELAY_TIME } from './constants/metricConstants';
 
-export const init = (apiKey: string): void => {
-  // eslint-disable-next-line no-console
-  console.log('Initialized with API key:', apiKey);
+export const init = (): void => {
+  const measurements = [measureFCP(), measureTTFB(), measureDOMLoad(), measureWindowLoad()];
 
-  measureFCP();
-  measureTTFB();
-  measureDOMLoad();
-  measureWindowLoad();
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-call,@typescript-eslint/no-unsafe-member-access,@typescript-eslint/no-floating-promises
+  Promise.allSettled(measurements).then((results: PromiseSettledResult<MetricModel>[]) => {
+    const measureTime = new Date().toISOString();
+    const successfulMeasurements = results.filter(({ status }) => status === 'fulfilled');
+    const metrics = successfulMeasurements.map(
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+      ({ value }: PromiseFulfilledResult<MetricModel>): MetricModel => ({
+        ...value,
+        measureTime,
+      })
+    );
+
+    let failedRequestCount = 0;
+
+    const sendMetricsToServer = () => {
+      postMetric(metrics).then(
+        () => {
+          // eslint-disable-next-line no-console
+          console.log('Metrics sent successfully to server.');
+
+          failedRequestCount = 0;
+        },
+        () => {
+          // eslint-disable-next-line no-console
+          console.log('Failed to send metrics, trying again...');
+
+          failedRequestCount += 1;
+
+          if (failedRequestCount < MAX_RETRY_COUNT_ON_NETWORK_FAULT) {
+            setTimeout(sendMetricsToServer, REQUEST_RETRY_DELAY_TIME);
+          }
+        }
+      );
+    };
+
+    sendMetricsToServer();
+  });
+
   measureResourceTimings();
 };
