2f20456c23a31842f07c742a54995f52095652a1
Onur Cem <onurcemsenel@gmail.com>
2020-09-22 00:52:20 +0300

    feat: add ESM module output

--- rollup.config.js
+++ rollup.config.js
@@ -2,18 +2,38 @@ import replace from '@rollup/plugin-replace';
 import typescript from '@rollup/plugin-typescript';
 import { terser } from 'rollup-plugin-terser';
 
-export default {
+const commonOptions = {
   input: 'src/index.ts',
-  output: {
-    format: 'umd',
-    file: './dist/perf-analytics.min.js',
-    name: 'perfAnalytics',
-  },
-  plugins: [
-    replace({
-      __API_URL__: process.env.API_URL || 'http://localhost:8080',
-    }),
-    typescript(),
-    terser(),
-  ],
 };
+
+const getPlugins = (module) => [
+  replace({
+    __API_URL__: process.env.API_URL || 'http://localhost:8080',
+  }),
+  typescript(),
+  terser({
+    module,
+    mangle: true,
+    compress: true,
+  }),
+];
+
+export default [
+  {
+    ...commonOptions,
+    output: {
+      format: 'esm',
+      file: './dist/perf-analytics.min.js',
+    },
+    plugins: getPlugins(true),
+  },
+  {
+    ...commonOptions,
+    output: {
+      format: 'umd',
+      file: './dist/perf-analytics.umd.min.js',
+      name: 'perfAnalytics',
+    },
+    plugins: getPlugins(false),
+  },
+];
